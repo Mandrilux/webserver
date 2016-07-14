afed141534d76cd86cfb813fbf2fb69d8eaccb74
heraud_b <baptiste.heraud@epitech.eu>
2016-07-14 21:30:53 +0200

    opti

--- free.c
+++ free.c
@@ -5,9 +5,9 @@ int	free_double_char(char **tab)
   int	i = -1;
 
   if (tab == NULL)
-    return (-1);
+    return -1;
   while (tab[++i] != NULL)
     free(tab[i]);
   free(tab);
-  return (1);
+  return 1;
 }
--- log.c
+++ log.c
@@ -5,26 +5,26 @@
 ** Login   <baptiste@epitech.net>
 **
 ** Started on  Fri Jul  1 11:33:51 2016
-** Last update Thu Jul 14 11:50:12 2016 
+** Last update Thu Jul 14 21:30:45 2016 
 */
 
 #include "data.h"
 
-int	log_error(char *msg, int status, char *file, char *flag, int display)
-{
-  FILE *fd;
-  char  *date;
+/* int	log_error(char *msg, int status, char *file, char *flag, int display) */
+/* { */
+/*   FILE *fd; */
+/*   char  *date; */
 
-  date = get_date_system();
-  if (display == 1)
-    printf("[%s] %s\n", date, msg);
-  if ((fd = fopen(file, "a+")) == NULL)
-    return (status);
-  fprintf(fd, "[%s] [%s] %s\n", flag, date, msg);
-  fclose(fd);
-  free(date);
-  return (status);
-}
+/*   date = get_date_system(); */
+/*   if (display == 1) */
+/*     printf("[%s] %s\n", date, msg); */
+/*   if ((fd = fopen(file, "a+")) == NULL) */
+/*     return status; */
+/*   fprintf(fd, "[%s] [%s] %s\n", flag, date, msg); */
+/*   fclose(fd); */
+/*   free(date); */
+/*   return (status); */
+/* } */
 
 int	log_error_file(char *flag, char *ip, char *file_err, char *log)
 {
@@ -33,11 +33,11 @@ int	log_error_file(char *flag, char *ip, char *file_err, char *log)
 
   date = get_date_system();
   if ((fd = fopen(log, "a+")) == NULL)
-      return (-1);
+      return -1;
   fprintf(fd, "[%s] [%s] [client %s] File does not exist: %s\n", date, flag, ip, file_err);
   fclose(fd);
   free(date);
-  return (1);
+  return 1;
 }
 
 int     log_access_file(char *ip, char *type, char*agent, char *log)
@@ -47,11 +47,11 @@ int     log_access_file(char *ip, char *type, char*agent, char *log)
 
   date = get_date_system();
   if ((fd = fopen(log, "a+")) == NULL)
-    return (-1);
+    return -1;
   fprintf(fd, "[%s] - - [%s] - %s - %s \n", ip, date, type, agent);
   fclose(fd);
   free(date);
-  return (1);
+  return 1;
 }
 
 char *get_date_system()
@@ -62,10 +62,6 @@ char *get_date_system()
 
   struct tm * tm = localtime(&now);
   strftime(date, sizeof date, "%B %d, %Y %H:%M:%S", tm);
-#ifdef __linux__
   tmp = strdup(date);
-#elif _WIN32
-  tmp = _strdup(date);
-#endif
-  return (tmp);
+  return tmp;
 }
