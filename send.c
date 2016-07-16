1de36b5dfdf74783c7a9f3f2548c1a860cb98bf7
heraud_b <baptiste.heraud@epitech.eu>
2016-07-16 12:12:15 +0200

    log_fix

--- send.c
+++ send.c
@@ -5,7 +5,7 @@
 ** Login   <baptiste.heraud@epitech.eu>
 **
 ** Started on  Thu Jul 14 11:24:09 2016
-** Last update Sat Jul 16 11:42:08 2016 
+** Last update Sat Jul 16 12:12:04 2016 
 */
 
 #include "data.h"
@@ -21,14 +21,20 @@ int	send_page(SOCKET csock, t_weft *data, char *ip)
   if (response == NULL)
     return -1;
   if (data->error == 200)
+    {
       sprintf(response,"%s%s\r\n","HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n", data->code_page);
+      log_access_file(ip, data->type, data->user_agent, LOG_ACCESS);
+    }
   else
+    {
     sprintf(response,"%s",  "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
 	    "<!DOCTYPE html><html><head><title>404 NO FOUND</title>"
 	    "<style>body { background-color: white }"
 	    "h1 { font-size:4cm; text-align: center; color: black;"
 	    "text-shadow: 0 0 2mm red}</style></head>"
 	    "<body><h1>Not Found</h1><hr><p> V.01 serv_baptiste</body></html>\r\n");
+    log_error_file("ERROR", ip, data->page, LOG_ERROR);
+    }
   write(csock, response, strlen(response));
   return 1;
 }
@@ -43,7 +49,7 @@ int	send_page(SOCKET csock, t_weft *data, char *ip)
 /* 	          " text-shadow: 0 0 2mm red}</style></head>" */
 /* 	"<body><h1>Goodbye, world!</h1></body></html>\r\n"; */
 /*       write(csock, response, sizeof(response) - 1); */
-/*       log_access_file(ip, data.type, data.user_agent, LOG_ACCESS); */
+
 /*       return (1); */
 /* } */
 
